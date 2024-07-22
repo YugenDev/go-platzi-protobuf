3f09d6ebde1f522bfe7ab017a7dda7b0d0d5f31d
yugoso8512 <yugoso8512@gmail.com>
2024-07-22 20:07:43 +0000

    first commit

---
+++ .idx/dev.nix
@@ -0,0 +1,31 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.go
+    pkgs.protobuf
+    pkgs.grpc-tools
+    pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
+    pkgs.protoc-gen-go
+    pkgs.grpc
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "golang.go"
+    ];
+    workspace = {
+      onCreate = {
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {
+    };
+  };
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/platzi/go-platzi-protobuf
+
+go 1.21
+
+toolchain go1.21.11
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.4.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
