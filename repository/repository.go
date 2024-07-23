30cf7bb73cec381cadfae372cb03adcc861fdbbd
yugoso8512 <yugoso8512@gmail.com>
2024-07-23 14:05:36 +0000

    Added repository config

---
+++ repository/repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/platzi/go-platzi-protobuf/models"
+)
+
+type Repository interface {
+	GetStudent(ctx context.Context, id string) (*models.Student, error)
+	SetStudent(ctx context.Context, student *models.Student) error
+}
+
+var implementation Repository
+
+func SetRepository(repository Repository) {
+	implementation = repository
+}
+
+
+func GetStudent(ctx context.Context, id string) (*models.Student, error) {
+	return implementation.GetStudent(ctx, id)
+}
+
+func SetStudent(ctx context.Context, student *models.Student) error {
+	return implementation.SetStudent(ctx, student)
+}
