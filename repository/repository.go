e131f989951debc66fe5590ee0b639287f071bd5
yugoso8512 <yugoso8512@gmail.com>
2024-07-23 16:26:45 +0000

    Added test struct methods to repository

--- repository/repository.go
+++ repository/repository.go
@@ -9,6 +9,8 @@ import (
 type Repository interface {
 	GetStudent(ctx context.Context, id string) (*models.Student, error)
 	SetStudent(ctx context.Context, student *models.Student) error
+	GetTest(ctx context.Context, id string) (*models.Test, error)
+	SetTest(ctx context.Context, test *models.Test) error
 }
 
 var implementation Repository
@@ -17,11 +19,18 @@ func SetRepository(repository Repository) {
 	implementation = repository
 }
 
-
 func GetStudent(ctx context.Context, id string) (*models.Student, error) {
 	return implementation.GetStudent(ctx, id)
 }
 
 func SetStudent(ctx context.Context, student *models.Student) error {
 	return implementation.SetStudent(ctx, student)
+}
+
+func GetTest(ctx context.Context, id string) (*models.Test, error) {
+	return implementation.GetTest(ctx, id)
+}
+
+func SetTest(ctx context.Context, test *models.Test) error {
+	return implementation.SetTest(ctx, test)
 }
